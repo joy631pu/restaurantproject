b82924bf0731bb97f1c5dea77c2954562709de43
joy631pu <97850075+joy631pu@users.noreply.github.com>
2022-11-24 00:06:33 -0800

    Some bugs are fixed

--- js/google-map.js
+++ js/google-map.js
@@ -2,21 +2,18 @@
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    
     var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
+   
     
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
+        
         zoom: 7,
 
-        // The latitude and longitude to center the map (always required)
+       
         center: myLatlng,
 
-        // How you would like to style the map. 
+        
         scrollwheel: false,
         styles: [
             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
